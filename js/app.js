1b581b8e86e7d4a6ebe4c3ae317aed094cd8ece2
Dbar90 <georilla90@yahoo.com>
2021-08-11 21:39:20 -0400

    cleaned up styling and added a button for about the game/started some js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("Connect 4")
+
+
+
+function showRules() {
+  if (rules.style.display === 'none') {
+     rules.style.display = 'flex'
+  } else {
+    rules.style.display = 'none'
+  }
+}
+const rules = document.querySelector('.rules')
+const about = document.querySelector('.about')
+about.addEventListener('click', showRules)
+rules.style.display = 'none'
