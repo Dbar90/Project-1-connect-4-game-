9962bfc59631485e34d5e820b8be47fd91035805
Dbar90 <georilla90@yahoo.com>
2021-08-14 17:50:35 -0400

    fixed row and column arrays

--- js/app.js
+++ js/app.js
@@ -11,27 +11,29 @@ const about = document.querySelector('.about')
 //Tile Elements
 const allTiles = document.querySelectorAll('.tile')
 
+
 //Arrays:
 //Drop Buttons
 const dropperArray = [dropper[0], dropper[1], dropper[2], dropper[3], dropper[4], dropper[5], dropper[6]]
 
 //Rows
-const row0 = allTiles[0], allTiles[1], allTiles[2], allTiles[3], allTiles[4], allTiles[5], allTiles[6]
-const row1 = allTiles[7], allTiles[8], allTiles[9], allTiles[10], allTiles[11], allTiles[12], allTiles[13]
-const row2 = allTiles[14], allTiles[15], allTiles[16], allTiles[17], allTiles[18], allTiles[19], allTiles[20]
-const row3 = allTiles[21], allTiles[22], allTiles[23], allTiles[24], allTiles[25], allTiles[26], allTiles[27]
-const row4 = allTiles[28], allTiles[29], allTiles[30], allTiles[31], allTiles[32], allTiles[33], allTiles[34]
-const row5 = allTiles[35], allTiles[36], allTiles[37], allTiles[38], allTiles[39], allTiles[40], allTiles[41]
+const row0 = [allTiles[0], allTiles[1], allTiles[2], allTiles[3], allTiles[4], allTiles[5], allTiles[6]]
+const row1 = [allTiles[7], allTiles[8], allTiles[9], allTiles[10], allTiles[11], allTiles[12], allTiles[13]]
+const row2 = [allTiles[14], allTiles[15], allTiles[16], allTiles[17], allTiles[18], allTiles[19], allTiles[20]]
+const row3 = [allTiles[21], allTiles[22], allTiles[23], allTiles[24], allTiles[25], allTiles[26], allTiles[27]]
+const row4 = [allTiles[28], allTiles[29], allTiles[30], allTiles[31], allTiles[32], allTiles[33], allTiles[34]]
+const row5 = [allTiles[35], allTiles[36], allTiles[37], allTiles[38], allTiles[39], allTiles[40], allTiles[41]]
 const rows = [row0, row1, row2, row3, row4, row5]
 
 //Columns
-const column0 = allTiles[35], allTiles[28], allTiles[21], allTiles[14], allTiles[7], allTiles[0]
-const column1 = allTiles[36], allTiles[29], allTiles[22], allTiles[15], allTiles[8], allTiles[1]
-const column2 = allTiles[37], allTiles[30], allTiles[23], allTiles[16], allTiles[9], allTiles[2]
-const column3 = allTiles[38], allTiles[31], allTiles[24], allTiles[17], allTiles[10], allTiles[3]
-const column4 = allTiles[39], allTiles[32], allTiles[25], allTiles[18], allTiles[11], allTiles[4]
-const column5 = allTiles[40], allTiles[33], allTiles[26], allTiles[19], allTiles[12], allTiles[5]
-const column6 = allTiles[41], allTiles[34], allTiles[27], allTiles[20], allTiles[13], allTiles[6]
+const column0 = [allTiles[35], allTiles[28], allTiles[21], allTiles[14], allTiles[7], allTiles[0]]
+const column1 = [allTiles[36], allTiles[29], allTiles[22], allTiles[15], allTiles[8], allTiles[1]]
+const column2 = [allTiles[37], allTiles[30], allTiles[23], allTiles[16], allTiles[9], allTiles[2]]
+const column3 = [allTiles[38], allTiles[31], allTiles[24], allTiles[17], allTiles[10], allTiles[3]]
+const column4 = [allTiles[39], allTiles[32], allTiles[25], allTiles[18], allTiles[11], allTiles[4]]
+const column5 = [allTiles[40], allTiles[33], allTiles[26], allTiles[19], allTiles[12], allTiles[5]]
+const column6 = [allTiles[41], allTiles[34], allTiles[27], allTiles[20], allTiles[13], allTiles[6]]
+const columns = [column0, column1, column2, column3, column4, column5, column6]
 
 
 
@@ -45,10 +47,10 @@ const column6 = allTiles[41], allTiles[34], allTiles[27], allTiles[20], allTiles
 //Event Listeners:
 //Drop Buttons
 for (const dropper of dropperArray) {
-  dropper.addEventListener('mouseover', (e)=> {
+  dropper.addEventListener('mouseover', ()=> {
     styleElem.innerHTML = '.drop-button:hover:after {background: red;}'
   })
-  dropper.addEventListener('mouseout', (e)=> {
+  dropper.addEventListener('mouseout', ()=> {
     styleElem.innerHTML = '.drop-button:hover:after {background: white;}'
   })
 }
